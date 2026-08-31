1f93ec49443ad233d7513f97133374be12d18152
sauravghule <saurav.ghule.za05@gmail.com>
2026-08-31 12:01:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/CA-Final-Exam-Tracker/',
+  base: '/CA-Final-Exam-Tracker-V2/',
 });
